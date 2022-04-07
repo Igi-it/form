3db41166d4552fb8d9c87ac87addcd1302a7c2cd
Igi-it <igi701580@gmail.com>
2022-04-07 20:27:42 +0200

    03 - Hotovo

--- server.js
+++ server.js
@@ -15,33 +15,33 @@ app.use(express.static('public'));
 
 /* Nastavení endpointu, na kterém server provede odpověď a pošle klientovi */
 app.get('/about', (req, res) => {
-    res.send('Webserver IT2')
+    res.send('Igor Večeřa')
     res.end();
 });
 
 /* Endpoint serveru očekávající dva parametry - user a password */
 /* Příklad URL: http://localhost:3000/secret?user=Pepa&password=007 */
 app.get('/secret', (req, res) => {
-    let user = req.query.user;
+    let username = req.query.username;
     let password = req.query.password;
-    res.send(`Uživatel: ${user} | heslo: ${password}`);
+    res.send(`Uživatel: ${username} | heslo: ${password}`);
     res.end();
 });
 
 app.get('/save', (req, res) => {
-    let player = req.query.player;
-    let points = req.query.points;
-    res.send(`Hráč: ${player} | body: ${points}`);
+    let username = req.query.username;
+    let password = req.query.password;
+    res.send(`Hráč: ${username} | body: ${password}`);
     res.end();
 });
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.post('/save', urlencodedParser, (req, res) => {
-    let player = req.body.player;
-    let points = req.body.points;
+    let username = req.body.username;
+    let password = req.body.password;
     let date = new Date();
-    let str = `${player},${points},${date.toLocaleDateString()},${date.toLocaleTimeString()}\n`;
+    let str = `${username},${password},${date.toLocaleDateString()},${date.toLocaleTimeString()}\n`;
     fs.appendFile('./data/result.csv', str, function(err) {
         if (err) {
             console.error(err);
@@ -59,7 +59,7 @@ app.get('/results', (req, res) => {
     .then(data => {
         console.log(data);
         // res.send(data);
-        res.render('results.pug', {'players':data, 'nadpis': 'Výsledky hráčů'});
+        res.render('results.pug', {'players':data, 'nadpis': 'Seznam uživatelů'});
     })
     .catch(err => {
         console.log(err);
